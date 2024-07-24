18f50859293b049834f29862ddb303a234d2df43
wallacy-1 <wwallacy@live.com>
2024-07-24 20:33:31 -0300

    feat: :lipstick: Add tailwindcss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Home from './pages/Home.page';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./pages/Home.page";
+import reportWebVitals from "./reportWebVitals";
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
