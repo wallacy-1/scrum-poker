b866cf0178acd9f7134557dd0e8a92e118c385cf
wallacy-1 <wwallacy@live.com>
2024-09-20 12:00:14 -0300

    build: :building_construction: adjust production .env socket_url

--- src/services/scrum-poker/webSocketService.tsx
+++ src/services/scrum-poker/webSocketService.tsx
@@ -1,7 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL =
-  process.env.NODE_ENV === "production" ? "" : "http://localhost:3001";
+const URL = process.env.REACT_APP_SOCKET_URL ?? "http://localhost:3001";
 
 const socket = io(URL, {
   transports: ["websocket"],
