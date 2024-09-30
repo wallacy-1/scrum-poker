c0538f59a4140c5e0f00efe35f69054ce7d5490c
wallacy-1 <wwallacy@live.com>
2024-09-30 18:39:14 -0300

    refactor: :wheelchair: replace role='status' with <output> for improved accessibility in Spinner component

--- src/components/molecules/spinner/index.tsx
+++ src/components/molecules/spinner/index.tsx
@@ -6,11 +6,14 @@ const Spinner = () => {
   return (
     <div
       className="fixed inset-0 z-[1000] flex items-center justify-center text-white bg-gray-700/85 transition-opacity duration-300"
-      role="status"
       aria-live="polite"
     >
       <div className="w-12 h-12 mr-4 border-4 border-white rounded-full border-t-transparent animate-spin" />
-      {title && <p className="text-2xl text-center text-white">{title}</p>}
+      {title && (
+        <output className="text-2xl text-center text-white" aria-live="polite">
+          {title}
+        </output>
+      )}
     </div>
   );
 };
